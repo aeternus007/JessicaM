c8ed579131607431eb74b00e917a539e7d8923fc
BuildTools <unconfigured@null.spigotmc.org>
2022-04-20 20:52:54 +0200

    een poging tot denken

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, render_template, request, redirect, url_for, g
 
 from utils import *
@@ -11,11 +12,24 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     conn = get_db(DATABASE, g)
-
-    df = query(conn, "SELECT * FROM Movie")[:5]
+    df = pd.DataFrame(query(conn, "SELECT * FROM Movie")[:5])
     # print(movies)
 
     return render_template('home.html', df=df)
+
+
+@app.route("/more_info", methods=['POST', 'GET'])
+def more_info():
+    conn = get_db(DATABASE, g)
+    df = None
+
+    if request.method == 'POST':
+        email = request.form['email']
+        df = if len(query(conn, f"SELECT * FROM Booking WHERE Email = {email}")) '''hier iets goeds''':
+    if not df:
+        df = pd.DataFrame(query(conn, "SELECT * FROM Movie"))
+
+    return render_template('more_info.html', df=df)
 
 
 if __name__ == '__main__':
